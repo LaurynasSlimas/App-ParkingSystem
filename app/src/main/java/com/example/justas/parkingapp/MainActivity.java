23497f793a19638a3a15b265fb4335534226dae7
Laurynas Šlimas <l.slimas95@gmail.com>
2016-11-15 22:50:37 +0200

    Add files via upload

--- app/src/main/java/com/example/justas/parkingapp/MainActivity.java
+++ app/src/main/java/com/example/justas/parkingapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.justas.parkingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    Button bLogout;
+    EditText etName, etUsername, etCarModel, etCarBrand, etBankAcc, etFamilyName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etCarModel = (EditText) findViewById(R.id.etCarModel);
+        etCarBrand = (EditText) findViewById(R.id.etCarBrand);
+        etBankAcc = (EditText) findViewById(R.id.etBankAcc);
+        etFamilyName = (EditText) findViewById(R.id.etFamilyName);
+        bLogout = (Button) findViewById(R.id.bLogout);
+
+        bLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bLogout:
+
+                startActivity(new Intent(this, login.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/justas/parkingapp/register.java
+++ app/src/main/java/com/example/justas/parkingapp/register.java
@@ -2,12 +2,39 @@ package com.example.justas.parkingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class register extends AppCompatActivity {
+public class register extends AppCompatActivity implements View.OnClickListener{
+
+    Button bRegister;
+    EditText etName, etUsername, etCarModel, etCarBrand, etBankAcc, etFamilyName, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etCarModel = (EditText) findViewById(R.id.etCarModel);
+        etCarBrand = (EditText) findViewById(R.id.etCarBrand);
+        etBankAcc = (EditText) findViewById(R.id.etBankAcc);
+        etFamilyName = (EditText) findViewById(R.id.etFamilyName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bRegister:
+
+                break;
+        }
     }
 }
